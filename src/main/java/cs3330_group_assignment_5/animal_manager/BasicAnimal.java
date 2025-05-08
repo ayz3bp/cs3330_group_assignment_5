8c13da632a1e198af19a1090fd84e102c1b8cf3c
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 16:48:09 -0500

    Update BasicAnimal.java

--- src/main/java/cs3330_group_assignment_5/animal_manager/BasicAnimal.java
+++ src/main/java/cs3330_group_assignment_5/animal_manager/BasicAnimal.java
@@ -1,1 +1,71 @@
+package cs3330_group_assignment_5.animal_manager;
 
+class BasicAnimal extends Pet {
+	int id;
+	String name;
+	String type;
+	String species;
+	int age;
+	Boolean adopted;
+	
+	public BasicAnimal() {}
+	
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String getType() {
+		return type;
+	}
+	
+	@Override
+	public String getSpecies() {
+		return species;
+	}
+	
+	@Override
+	public int getAge() {
+		return age;
+	}
+	
+	@Override
+	public Boolean getAdopted() {
+		return adopted;
+	}
+	
+	@Override
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	@Override
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	
+	@Override
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public void setAdopted(Boolean adopted) {
+		this.adopted = adopted;
+	}
+}
