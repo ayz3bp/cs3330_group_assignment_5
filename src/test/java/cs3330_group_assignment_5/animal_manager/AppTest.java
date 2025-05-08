f2e69097b330bff38af3fa0717f2ebcba5b72541
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 18:31:13 -0500

    Create AppTest.java

---
+++ src/test/java/cs3330_group_assignment_5/animal_manager/AppTest.java
@@ -0,0 +1,13 @@
+package cs3330_group_assignment_5.animal_manager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
