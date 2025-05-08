35f6f0833ea96ee15ea43fc284251d8064094c75
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 18:26:52 -0500

    Create Shelter.java

---
+++ src/main/java/cs3330_group_assignment_5/animal_manager/Shelter.java
@@ -0,0 +1,32 @@
+package cs3330_group_assignment_5.animal_manager;
+
+import java.util.*;
+
+class Shelter<T extends Pet> {
+	List<T> shelterList;
+	
+	public Shelter() {
+		this.shelterList = new ArrayList<>();
+	}
+	
+	public List<T> getShelterList(){
+		return shelterList;
+	}
+	
+	public void addPet(T newPet) {
+		shelterList.add(newPet);
+	}
+
+	public void sortShelterByName() {
+		Collections.sort(shelterList);
+	}
+
+	public void sortShelterByAge() {
+		Collections.sort(shelterList, new Age());
+	}
+
+	public void sortShelterBySpecies() {
+		Collections.sort(shelterList, new Species());
+	}
+	
+}
