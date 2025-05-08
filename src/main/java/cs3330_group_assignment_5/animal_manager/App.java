a8ea5eb34554dfee0abf55234c57280ffcd53f58
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 18:47:31 -0500

    Update App.java

--- src/main/java/cs3330_group_assignment_5/animal_manager/App.java
+++ src/main/java/cs3330_group_assignment_5/animal_manager/App.java
@@ -1,11 +1,17 @@
 package cs3330_group_assignment_5.animal_manager;
 
 public class App {
+  
   public static void main(String[] args) {
-    ShelterView view = new ShelterView();
+
     Shelter shelter = new Shelter();
+    
+    ShelterView view = new ShelterView();
+    
     ShelterController controllerTest = new ShelterController(shelter, view);
+
     controllerTest.initShelter();
+    
     controllerTest.initView();
   }
 }
