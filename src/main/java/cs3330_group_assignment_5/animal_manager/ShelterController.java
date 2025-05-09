3801b308884c75a8564895ad687bfacd630fc40c
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 19:12:00 -0500

    Update ShelterController.java

--- src/main/java/cs3330_group_assignment_5/animal_manager/ShelterController.java
+++ src/main/java/cs3330_group_assignment_5/animal_manager/ShelterController.java
@@ -10,166 +10,188 @@ import java.util.*;
 import java.util.regex.*;
 
 import javax.swing.JOptionPane;
-
 import com.google.gson.*;
 
 class ShelterController {
-	private Shelter<Pet> shelter;
-	private ShelterView shelterView;
-	
-	public ShelterController(Shelter<Pet> shelter, ShelterView shelterView) {
-		this.shelter = shelter;
-		this.shelterView = shelterView;
-	}
+    
+    private Shelter<Pet> animalShelter;
+    private ShelterView shelterDisplay;
+    
+    public ShelterController(Shelter<Pet> animalShelter, ShelterView shelterDisplay) {
+        this.animalShelter = animalShelter;
+        this.shelterDisplay = shelterDisplay;
+    }
 
-	public void initShelter() {
-		Gson gson = new Gson();
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		try(InputStream pets = classLoader.getResourceAsStream("pets.json")){
-			Reader reader = new InputStreamReader(pets, "UTF-8");
-			shelter.shelterList = new ArrayList<>(Arrays.asList(gson.fromJson(reader, BasicAnimal[].class)));
-			
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		try(InputStream pets = classLoader.getResourceAsStream("exotic_animals.json")){
-			Reader reader = new InputStreamReader(pets, "UTF-8");
-			List<ExoticAnimal> tempList = Arrays.asList(gson.fromJson(reader, ExoticAnimal[].class));
-			for(int i = 0; i < tempList.size(); i++) {
-				AnimalAdapter temp = new AnimalAdapter(tempList.get(i));
-				Pet tempPet = Collections.max(shelter.shelterList, new Id());
-				temp.setId(tempPet.getId() + 1);
-				shelter.addPet(temp);
-			}
-			
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    public void initShelter() {
+        Gson jsonParser = new Gson();
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        
+        // Load regular pets
+        try (InputStream petStream = loader.getResourceAsStream("pets.json")) {
+            Reader fileReader = new InputStreamReader(petStream, "UTF-8");
+            animalShelter.shelterList = new ArrayList<>(Arrays.asList(jsonParser.fromJson(fileReader, BasicAnimal[].class)));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        
+        // Load exotic animals
+        try (InputStream exoticStream = loader.getResourceAsStream("exotic_animals.json")) {
+            Reader fileReader = new InputStreamReader(exoticStream, "UTF-8");
+            List<ExoticAnimal> exoticAnimals = Arrays.asList(jsonParser.fromJson(fileReader, ExoticAnimal[].class));
+            
+            for (int i = 0; i < exoticAnimals.size(); i++) {
+                AnimalAdapter adaptedAnimal = new AnimalAdapter(exoticAnimals.get(i));
+                Pet lastPet = Collections.max(animalShelter.shelterList, new Id());
+                adaptedAnimal.setId(lastPet.getId() + 1);
+                animalShelter.addPet(adaptedAnimal);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
   
-	public void initView() {
-		shelterView.initShelterView(shelter.shelterList);
-		shelterView.writeButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		LocalDateTime curDateTime = LocalDateTime.now();
-        		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
-        		String dateTime = curDateTime.format(format);
-        		String fileName = dateTime + "_pets.json";
-        		Gson builder = new GsonBuilder().setPrettyPrinting().create();
-        		try(FileWriter write = new FileWriter(fileName)){
-        			for(int i = 0; i < shelter.shelterList.size(); i++) {
-        				builder.toJson(shelter.shelterList.get(i), write);
-        			}
-        		}catch(IOException g) {
-        			g.printStackTrace();
-        		}
-        	}
+    public void initView() {
+        shelterDisplay.initShelterView(animalShelter.shelterList);
+        
+        // Export to JSON button
+        shelterDisplay.writeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                LocalDateTime currentTime = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
+                String timestamp = currentTime.format(formatter);
+                String outputFile = timestamp + "_pets.json";
+                
+                Gson prettyPrinter = new GsonBuilder().setPrettyPrinting().create();
+                try (FileWriter writer = new FileWriter(outputFile)) {
+                    for (int i = 0; i < animalShelter.shelterList.size(); i++) {
+                        prettyPrinter.toJson(animalShelter.shelterList.get(i), writer);
+                    }
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
         });
-		shelterView.button.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		shelter.sortShelterByName();
-        		sortViewList();
-        	}
+        
+        // Sort by name button
+        shelterDisplay.button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                animalShelter.sortShelterByName();
+                refreshDisplayList();
+            }
         });
-		shelterView.ageButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		shelter.sortShelterByAge();
-        		sortViewList();
-        	}
+        
+        // Sort by age button
+        shelterDisplay.ageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                animalShelter.sortShelterByAge();
+                refreshDisplayList();
+            }
         });
-		shelterView.specButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		shelter.sortShelterBySpecies();
-        		sortViewList();
-        	}
+        
+        // Sort by species button
+        shelterDisplay.specButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                animalShelter.sortShelterBySpecies();
+                refreshDisplayList();
+            }
         });
-		shelterView.remButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		int removed = 0;
-        		for(int i = 0; i < shelter.shelterList.size(); i++) {
-        			if(shelter.shelterList.get(i).getName().compareTo(shelterView.name.getText()) == 0 
-        					&& shelter.shelterList.get(i).getType().compareTo(shelterView.type.getText()) == 0 
-        					&& shelter.shelterList.get(i).getSpecies().compareTo(shelterView.species.getText()) == 0 
-        					&& shelter.shelterList.get(i).getAge() == Integer.parseInt(shelterView.age.getText())) {
-        				shelter.shelterList.remove(i);
-        				removed = 1;
-        			}
-        		}
-        		if(removed != 1) {
-        			JOptionPane.showMessageDialog(null, "Item not in List!");
-        		}
-        		sortViewList();
-        	}
-        	
+        
+        // Remove pet button
+        shelterDisplay.remButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int removalStatus = 0;
+                for (int i = 0; i < animalShelter.shelterList.size(); i++) {
+                    Pet currentPet = animalShelter.shelterList.get(i);
+                    if (currentPet.getName().compareTo(shelterDisplay.name.getText()) == 0 
+                            && currentPet.getType().compareTo(shelterDisplay.type.getText()) == 0 
+                            && currentPet.getSpecies().compareTo(shelterDisplay.species.getText()) == 0 
+                            && currentPet.getAge() == Integer.parseInt(shelterDisplay.age.getText())) {
+                        animalShelter.shelterList.remove(i);
+                        removalStatus = 1;
+                    }
+                }
+                
+                if (removalStatus != 1) {
+                    JOptionPane.showMessageDialog(null, "Item not found");
+                }
+                refreshDisplayList();
+            }
         });
-		shelterView.adoptButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		int index = shelterView.list.getSelectedIndex();
-        		if(shelter.getShelterList().get(index).getAdopted() == false) {
-        			shelter.getShelterList().get(index).setAdopted(true);
-        			shelterView.adoptLabel.setText("Not Available");
-					shelterView.adoptLabel.setForeground(Color.RED);
-        		}
-        		else {
-        			JOptionPane.showMessageDialog(null, "Already Adopted!");
-        		}
-        	}
+        
+        // Adopt pet button
+        shelterDisplay.adoptButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int selectedIndex = shelterDisplay.list.getSelectedIndex();
+                Pet selectedPet = animalShelter.getShelterList().get(selectedIndex);
+                
+                if (!selectedPet.getAdopted()) {
+                    selectedPet.setAdopted(true);
+                    shelterDisplay.adoptLabel.setText("Unavailable");
+                    shelterDisplay.adoptLabel.setForeground(Color.RED);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Adopted already");
+                }
+            }
         });
-		shelterView.addButton.addActionListener(new ActionListener() {
-        	
-        	@Override
-        	public void actionPerformed(ActionEvent e) {
-        		int addVal = 0;
-        		for(int i = 0; i < shelter.shelterList.size(); i++) {
-        			if(shelter.shelterList.get(i).getName().compareTo(shelterView.name.getText()) == 0 
-        					&& shelter.shelterList.get(i).getType().compareTo(shelterView.type.getText()) == 0 
-        					&& shelter.shelterList.get(i).getSpecies().compareTo(shelterView.species.getText()) == 0 
-        					&& shelter.shelterList.get(i).getAge() == Integer.parseInt(shelterView.age.getText())) {
-        				JOptionPane.showMessageDialog(null, "Already in the shelter!");
-        				addVal = 1;
-        			}
-        		}
-        		try {
-	        		if(addVal == 0) {
-	        			if(shelterView.name.getText().matches("[a-zA-Z]+")) {
-		        			BasicAnimal temp = new BasicAnimal();
-		        			Pet tempPet = Collections.max(shelter.shelterList, new Id());
-		    				temp.setId(tempPet.getId() + 1);
-		    				String capName = shelterView.name.getText().substring(0,1).toUpperCase() + shelterView.name.getText().substring(1);
-		        			temp.setName(capName);
-		        			temp.setType(shelterView.type.getText());
-		        			temp.setSpecies(shelterView.species.getText());
-		        			temp.setAge(Integer.parseInt(shelterView.age.getText()));
-		        			temp.setAdopted(false);
-		        			shelter.addPet(temp);
-		        			sortViewList();
-	        			}
-	        			else {
-	        				JOptionPane.showMessageDialog(null, "Error: Please Ensure Fields Contain Only Alphabetical Characters And No Spaces");
-	        			}
-	        		}
-        		}catch(Exception f) {
-        			JOptionPane.showMessageDialog(null, "Error: Please Ensure Fields Contain Only Alphabetical Characters And No Spaces");
-        			f.printStackTrace();
-        		}
-        	}
+        
+        // Add pet button
+        shelterDisplay.addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int duplicateCheck = 0;
+                
+                // Check if pet already exists
+                for (int i = 0; i < animalShelter.shelterList.size(); i++) {
+                    Pet existingPet = animalShelter.shelterList.get(i);
+                    if (existingPet.getName().compareTo(shelterDisplay.name.getText()) == 0 
+                            && existingPet.getType().compareTo(shelterDisplay.type.getText()) == 0 
+                            && existingPet.getSpecies().compareTo(shelterDisplay.species.getText()) == 0 
+                            && existingPet.getAge() == Integer.parseInt(shelterDisplay.age.getText())) {
+                        JOptionPane.showMessageDialog(null, "In the shelter");
+                        duplicateCheck = 1;
+                    }
+                }
+                
+                try {
+                    if (duplicateCheck == 0) {
+                        if (shelterDisplay.name.getText().matches("[a-zA-Z]+")) {
+                            BasicAnimal newAnimal = new BasicAnimal();
+                            Pet lastPet = Collections.max(animalShelter.shelterList, new Id());
+                            newAnimal.setId(lastPet.getId() + 1);
+                            
+                            // Capitalize first letter of name
+                            String formattedName = shelterDisplay.name.getText().substring(0, 1).toUpperCase() 
+                                    + shelterDisplay.name.getText().substring(1);
+                            
+                            newAnimal.setName(formattedName);
+                            newAnimal.setType(shelterDisplay.type.getText());
+                            newAnimal.setSpecies(shelterDisplay.species.getText());
+                            newAnimal.setAge(Integer.parseInt(shelterDisplay.age.getText()));
+                            newAnimal.setAdopted(false);
+                            
+                            animalShelter.addPet(newAnimal);
+                            refreshDisplayList();
+                        } else {
+                            JOptionPane.showMessageDialog(null, 
+                                    "Only alphabetical characters are allowed, and no spaces");
+                        }
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, 
+                            "Only alphabetical characters are allowed, and no spaces");
+                    ex.printStackTrace();
+                }
+            }
         });
-	}
-	public void sortViewList() {
-		shelterView.updateList(shelter.shelterList);
-	}
+    }
+    
+    public void refreshDisplayList() {
+        shelterDisplay.updateList(animalShelter.shelterList);
+    }
 }
