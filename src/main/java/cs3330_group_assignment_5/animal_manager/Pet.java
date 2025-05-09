17699ffa96719ce94558c65679511ec98b19c04f
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 19:03:31 -0500

    Update Pet.java

--- src/main/java/cs3330_group_assignment_5/animal_manager/Pet.java
+++ src/main/java/cs3330_group_assignment_5/animal_manager/Pet.java
@@ -3,26 +3,41 @@ package cs3330_group_assignment_5.animal_manager;
 import java.util.*;
 
 abstract class Pet implements java.lang.Comparable<Pet>{
-	public abstract String getName();
-	public abstract String getType();
-	public abstract String getSpecies();
+	
 	public abstract int getAge();
-	public abstract Boolean getAdopted();
+
 	public abstract int getId();
-	public abstract void setName(String name);
-	public abstract void setType(String type);
-	public abstract void setSpecies(String species);
+	
 	public abstract void setAge(int age);
+	
 	public abstract void setAdopted(Boolean adopted);
+	
 	public abstract void setId(int id);
 	
+	public abstract String getName();
+	
+	public abstract String getType();
+	
+	public abstract String getSpecies();
+	
+	public abstract void setName(String name);
+	
+	public abstract void setType(String type);
+	
+	public abstract void setSpecies(String species);
+	
+	public abstract Boolean getAdopted();
+	
 	public int compareTo(Pet compPet) {
-		String name = getName();
 		String compName = compPet.getName();
+		String name = getName();
+		
 		int compVal = name.compareTo(compName);
+		
 		if(compVal != 0) {
 			return compVal;
 		}
+			
 		else {
 			return 0;
 		}
@@ -30,20 +45,31 @@ abstract class Pet implements java.lang.Comparable<Pet>{
 	}
 }
 
-class Age implements Comparator<Pet>{
+class Id implements Comparator<Pet>{
+	
 	public int compare(Pet pet, Pet compPet) {
+		
+		return Integer.compare(pet.getId(), compPet.getId());
+		
+	}
+}
+
+class Age implements Comparator<Pet>{
+	
+	public int compare(Pet pet, Pet compPet) {
+		
 		return Double.compare(pet.getAge(), compPet.getAge());
+		
 	}
 }
 
 class Species implements Comparator<Pet>{
+	
 	public int compare(Pet pet, Pet compPet) {
+		
 		return pet.getSpecies().compareTo(compPet.getSpecies());
+		
 	}
 }
 
-class Id implements Comparator<Pet>{
-	public int compare(Pet pet, Pet compPet) {
-		return Integer.compare(pet.getId(), compPet.getId());
-	}
-}
+
