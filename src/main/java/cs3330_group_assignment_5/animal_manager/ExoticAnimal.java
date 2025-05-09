31e766378bec7e8289a23e1126de1ea680b04582
ayz3bp <ayz3bp@umsystem.edu>
2025-05-08 19:00:23 -0500

    Update ExoticAnimal.java

--- src/main/java/cs3330_group_assignment_5/animal_manager/ExoticAnimal.java
+++ src/main/java/cs3330_group_assignment_5/animal_manager/ExoticAnimal.java
@@ -1,42 +1,19 @@
 package cs3330_group_assignment_5.animal_manager;
 
 class ExoticAnimal {
-	String uniqueId;
+	String uniqueNum;
+	
 	String animalName;
+	
 	String category;
+	
 	String subSpecies;
+	
 	int yearsOld;
 	
 	public ExoticAnimal() {}
 	
-	public String getUniqueId() {
-		return uniqueId;
-	}
-
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
-
-	public String getAnimalName() {
-		return animalName;
-	}
-
-	public void setAnimalName(String animalName) {
-		this.animalName = animalName;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getSubSpecies() {
-		return subSpecies;
-	}
-
+	
 	public void setSubSpecies(String subSpecies) {
 		this.subSpecies = subSpecies;
 	}
@@ -48,4 +25,35 @@ class ExoticAnimal {
 	public void setYearsOld(int yearsOld) {
 		this.yearsOld = yearsOld;
 	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setUniqueId(String uniqueId) {
+		this.uniqueId = uniqueId;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public String getUniqueId() {
+		return uniqueId;
+	}
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getSubSpecies() {
+		return subSpecies;
+	}
+
+
+
 }
